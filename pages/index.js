f6f62b925384de4b8f487e9c17043b642229ca66
kaiquemaia <kaiquefeng1@gmail.com>
2021-01-28 23:11:48 -0300

    feat: Criando primeira estrutura da aplicacao

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,42 @@
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+            <Widget.Header>
+              <h1>{db.title}</h1>
+            </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quiz da galera</h1>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+        <GitHubCorner projectUrl="https://github.com/kaiquemaia"/>
+      </QuizContainer>
+    </QuizBackground>
+  )
 }
